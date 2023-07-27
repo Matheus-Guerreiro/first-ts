87b462023c1ee96d074d07fb3c15e324ce501888
Matheus <matheusglloureiro@gmail.com>
2023-07-27 10:38:28 -0300

    .

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+enum States {
+    MG = "Minas Gerais",
+    SP = "São Paulo"
+}
+
+interface IAddress {
+    street:string;
+    number:number;
+    state:States;
+}
+
+interface IProducts {
+    id: number;
+    name: string;
+    price: number;
+    description?: string;
+    address: IAddress;
+}
+
+let products: IProducts[] = []
+
+
+app.get('/produtos', (req: Request, res: Response) => {
+
+
+    const newProduct: IProducts = {
+        id: Math.random(),
+        name: "Batedeira",
+        price: 300,
+        description: "Batedeira muito boa",
+        address: {
+            street: "Rua dos bobos",
+            number: 0,
+            state: States.SP
+        }
+    }
+    products.push(newProduct)
+
+
+    return res.json(products)
+})
+
+app.listen(3000, () => {
+    console.log('Servidor rodando')
+})
